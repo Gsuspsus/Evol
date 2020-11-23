33b66baf0b2d1db6ab44e518c11dbab866e99ba6
gstok123 <gstok123@gmail.com>
2020-11-23 13:55:32 +0100

    updated user interface

--- Main.hs
+++ Main.hs
@@ -1,11 +1,16 @@
-import Front
 import System.Random
 import NoiseMapGen
+import Control.Monad
 import TileMapGen
-
+import Front
 main :: IO ()
 main = do
     g <- newStdGen
     let dim = (50,100)
     let ms = noiseMap (g,ridge g) dim
     display $ generate ms
+    a <- getLine
+    forever $ do
+        let ms = noiseMap (g,ridge g) dim
+        display $ generate ms
+        main
