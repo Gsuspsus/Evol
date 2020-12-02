5eab088c9d96bdeb0fede9100cb56364d00ef72a
gfalkeenthros <58170723+gfalkeenthros@users.noreply.github.com>
2020-12-02 12:01:51 +0100

    idk

--- Main.hs
+++ Main.hs
@@ -9,7 +9,7 @@ import WorldGen
 
 main :: IO ()
 main = do
-  maybeSize <- getTerminalSize
+  maybeSize <- getTermnalSize
   let dim = bimap fromIntegral fromIntegral $ fromMaybe (80, 100) maybeSize
   forever $ do
     g <- newStdGen
